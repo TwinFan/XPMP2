2cb3e8d7bee79437efd971797567968ed94212ba
TwinFan <twinfan@sonux.net>
2026-03-01 16:54:17 +0100

    build/win: Build and deploy a debug version of the lib, closes #79

--- .github/actions/build-win/build-win.cmd
+++ .github/actions/build-win/build-win.cmd
@@ -1,13 +1,15 @@
 @ECHO OFF
 
-REM We expect 2 parameters
+REM We expect at least 3 parameters
 REM Additional parameters are passed through to CMake
 IF "%~1"=="" GOTO :PRINT_HELP
 IF "%~2"=="" GOTO :PRINT_HELP
+IF "%~3"=="" GOTO :PRINT_HELP
 
 SETLOCAL
-SET MY_VC_BASE=%~1
-SET MY_BUILD_DIR=%~2
+SET "MY_VC_BASE=%~1"
+SET "MY_BUILD_DIR=%~2"
+SET "MY_BUILD_TYPE=%~3"
 
 REM Set VC environment variables
 CALL "%MY_VC_BASE%\VC\Auxiliary\Build\vcvarsall.bat" x64
@@ -21,7 +23,7 @@ PUSHD "%MY_BUILD_DIR%"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 REM Create Makefile from CMakeLists.txt, using NMake output beacause NMake happens to be available
-CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING="RelWithDebInfo" -DCMAKE_MAKE_PROGRAM="nmake.exe" -DCMAKE_TOOLCHAIN_FILE="..\docker\Toolchain-msvc-x86-64.cmake" %~3 %~4 %~5 ..
+CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE:STRING="%MY_BUILD_TYPE%" -DCMAKE_MAKE_PROGRAM="nmake.exe" -DCMAKE_TOOLCHAIN_FILE="..\docker\Toolchain-msvc-x86-64.cmake" %~4 %~5 %~6 ..
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 REM Perform the actual build
@@ -33,9 +35,11 @@ POPD
 EXIT /B 0
 
 :PRINT_HELP
-ECHO Call with two parameters:
+ECHO Call with at least 3 parameters:
 ECHO 1. Base directory for Visual Studio Build Tools, something like
 ECHO    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" or
 ECHO    "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools";
 ECHO 2. Build directory, usually something local like "build-win".
+ECHO 3. CMAKE_BUILD_TYPE, like "RelWithDebInfo" or "Debug"
+ECHO Optional parameters 4~6 are passed through to the CMAKE call.
 EXIT /B 1
